a605a84f9418450332598d7615d6620bc8e8abd0
Baslandze Lasha <lasha.qa@mail.ru>
2023-05-15 00:16:47 +0300

    update//

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 @pytest.fixture(autouse=True)
 def browser_open():
-    browser.open_url('https://demoqa.com')
+    browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 700
     browser.config.window_height = 1080
     yield
